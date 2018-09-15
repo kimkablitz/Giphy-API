d16e7812d7bfb04f398eec69fcaef9b011b1f5ae
kimkablitz <kim.tran549@gmail.com>
2018-09-14 18:45:39 -0700

    working buttons

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    var animals = ["shark", "panda", "tiger", "cat", "dog", "owl", "rat", "snake", "aligator", "zebra", "monkey", "bear", "tiger", "hippo", "elephant", "frog", "butterfly"];
-
+    var animals = ["shark", "panda", "tiger", "cat", "dog", "owl", "rat", "snake", "aligator", "zebra", "monkey", "bear", "hippo", "elephant", "frog", "butterfly"];
+    var userInput;
     function displayBtn() {
         $("#gifBtn").empty();
         //create a button when user inputs 
@@ -14,12 +14,29 @@ $(document).ready(function () {
         }
     }
     //and hit add
-    $('#addGif').on("click", function (e) {
-        e.preventDefault();
-        var userInput = $('#animalName-input').val().trim();
-        animals.push(userInput);
-        displayBtn();
-    });
+    function addBtn() {
+        $('#addGif').on("click", function (e) {
+            e.preventDefault();
+            var userInput = $('#animalName-input').val().trim();
+            if (userInput !== "") {
+                animals.push(userInput);
+                displayBtn();
+            }
+            else {
+                alert("Must enter a valid input to proceed!")
+            }
+
+        });
+    }
+    addBtn()
+    function removeBtn() {
+        $('#removeGif').on("click", function () {
+            animals.pop(userInput);
+            displayBtn()
+            // return false;
+        })
+    }
+    removeBtn()
 
     $(document).on("click", ".animal", displayAnimalGif)
     displayBtn();
@@ -34,26 +51,54 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).done(function (response) {
+            console.log(response)
             $("#gifsHere").empty()
-            var animalGifDiv = $("<div class='theActualGifsBox'>")
+            var animalGifDiv = $('<div class="animalGifDiv">')
             var r = response.data
-            //loopthrough the data object
-            for(var i=0; i<r.length;i++){
-                $(".theActualGifsBox").append(r[i].rating)
-            
-            var rating = r[i].rating
-            var rate = $("<p>").text("Rating: " + rating)
-            animalGifDiv.append(rate)
-            //append the gif url
-            var gifURL = r[i].images.fixed_height_small.url;
-            var gif = $("<img>").attr("src", gifURL)
-            animalGifDiv.append(gif)
-            $("#gifsHere").prepend(animalGifDiv)
-            }
 
+            //loopthrough the data object
+            for (var i = 0; i < r.length; i++) {
+
+                var rating = r[i].rating
+                var rate = $("<p>").text("Rating: " + rating)
+                animalGifDiv.append(rate)
+                //append the gif urls
+                var gifImage = $("<img>")
+                gifImage.attr("src", r[i].images.fixed_height_still.url);
+                gifImage.attr("data-still", r[i].images.fixed_height_still.url);
+                gifImage.attr("data-animate", r[i].images.fixed_height.url);
+                gifImage.attr("data-state", "still");
+                gifImage.addClass("image");
+                
+                animalGifDiv.append(gifImage)
+                $("#gifsHere").prepend(animalGifDiv)
+            }
         })
-    
     }
+    //pausing gifts
+    $(document).on("click", ".animal", displayAnimalGif);
+    // $(document).on("click",".image", function(){
+    //     var state = $(this).attr('data-state')
+    //     if(state = 'still'){
+    //         $(this).attr('src',$(this).attr('data-animate'))
+    //         $(this).attr('data-state','animate')
+    //     }else{
+    //         $(this).attr('src',$(this).attr('data-still'))
+    //         $(this).attr('data-state','still')
+
+    //     }
+    // })
+
+    $(document).on("click", ".image", function () {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    })
 
 
 
